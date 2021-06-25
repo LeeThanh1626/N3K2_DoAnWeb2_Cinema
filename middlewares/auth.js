3423a62a85ffde1b63cf45c075af98dacc794e29
Minh@nh0302 <18600232@student.hcmus.edu.vn>
2021-06-25 23:51:48 +0700

    -find = done user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,16 +3,16 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/user');
 
 module.exports = asyncHandler(async function auth(req, res, next) {
-    // const { userId } = req.session;
-    // res.locals.currentUser = null;
-    // if (userId) {
-    //     const user = await User.findById(userId)
-    //     if (user) {
-    //         req.currentUser = user;
-    //         res.locals.currentUser = user;
-    //     }
-    //     next();
-    // } else {
-    //     next();
-    // }
+    const { userId } = req.session;
+    res.locals.currentUser = null;
+    if (userId) {
+        const user = await User.findById(userId)
+        if (user) {
+            req.currentUser = user;
+            res.locals.currentUser = user;
+        }
+        next();
+    } else {
+        next();
+    }
 });
