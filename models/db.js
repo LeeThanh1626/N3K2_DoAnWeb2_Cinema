fabc7632479de84d40a753566cbd99bfa3010b23
Minh@nh0302 <18600232@student.hcmus.edu.vn>
2021-07-17 11:17:30 +0700

    fix again

--- models/db.js
+++ models/db.js
@@ -1,10 +1,9 @@
 const { Sequelize } = require('sequelize');
 module.exports = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:beson0302@localhost:5432/Doan', {
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                rejectUnauthorized: false,
-            }
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false,
         }
-    })
-    //fix database
+    }
+})
